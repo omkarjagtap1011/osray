90e012abf1260ae06245c7ff5f37b821cdcc51bf
omkarjagtap1011 <126784997+omkarjagtap1011@users.noreply.github.com>
2023-09-14 19:20:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { google } = require('googleapis');
-const fs = require('fs');
-
 const app = express();
 const port = process.env.PORT || 3000;
-
-// Load the service account key JSON file
-const serviceAccountKey = require('./credentials.json'); // Replace with your service account key file
 
 // Set up middleware
 app.use(bodyParser.json());
@@ -25,72 +19,9 @@ app.get('/', (req, res) => {
     });
 });
 
-// Handle form submissions
-app.post('/submit', async (req, res) => {
-    const { name, enrollment, os } = req.body;
-
-    try {
-        // Authenticate with Google Sheets using the service account credentials
-        const auth = await authorize();
-
-        const osLower = os.toLowerCase();
-
-        // Check if OS is unique
-        const isUnique = await checkUniqueOS(auth, osLower);
-
-        if (!isUnique) {
-            res.json({ error: 'Operating system is not unique.' });
-            return;
-        }
-
-        // Add data to Google Sheets
-        await appendToSheet(auth, [name, enrollment, osLower]);
-
-        res.json({ success: true });
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).json({ error: 'Server error.' });
-    }
-});
-
-// Google Sheets API functions
-async function authorize() {
-    const jwtClient = new google.auth.JWT(
-        serviceAccountKey.client_email,
-        null,
-        serviceAccountKey.private_key,
-        ['https://www.googleapis.com/auth/spreadsheets']
-    );
-
-    try {
-        await jwtClient.authorize();
-        return jwtClient;
-    } catch (err) {
-        console.error('Error authorizing with service account:', err);
-        throw err;
-    }
-}
-
-async function checkUniqueOS(auth, os) {
-    // Replace with your Google Sheets document ID and sheet name
-    const spreadsheetId = '1Go2YQHRPmf8cni6kINGj_QNc1HXT0VcVYp3Cg5x8Sp4'; // Update with your Spreadsheet ID
-    const sheetName = 'Sheet1';
-
-    // Load the sheet
-    const sheetsAPI = google.sheets({ version: 'v4', auth });
-    const sheet = await sheetsAPI.spreadsheets.values.get({
-        spreadsheetId,
-        range: sheetName,
-    });
-
-    // Check if the OS already exists in the sheet
-    const data = sheet.data.values;
-    if (data) {
-        return !data.some(row => row[2] === os);
-    } else {
-        return true;
-    }
-}
+// Include the submit route from submit.js
+const submitRoute = require('./functions/submit');
+app.use(submitRoute);
 
 // Start the server only if not in a Netlify environment
 if (!process.env.NETLIFY) {
